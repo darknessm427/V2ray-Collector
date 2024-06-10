58c1d0ce45c7672b39742337ffeba00c9a20a899
darknessm427 <darknessm.427@gmail.com>
2024-06-10 09:42:20 +0330

    Update collector.php

--- collector.php
+++ collector.php
@@ -1,54 +1,56 @@
 <?php
 
-$url = 'https://raw.githubusercontent.com/darknessm427/V2rayCollector/main/vmess_iran.txt';
-$filePath = 'sub/vmess';
+$urlvmess = 'https://raw.githubusercontent.com/darknessm427/V2rayCollector/main/vmess_iran.txt';
+$filePathvmess = 'sub/vmess';
 
-$content = file_get_contents($url);
 
-if ($content !== false) {
+$contentvmess = file_get_contents($urlvmess);
 
-    $content = preg_replace('/@.*/', '', $content);
+if ($contentvmess !== false) {
+
+    $contentvmess = preg_replace('/@.*/', '', $contentvmess);
 
     function changeNameInVmessLink($vmessLink) {
-        $jsonPart = base64_decode(substr($vmessLink, strpos($vmessLink, '://') + 3));
-        $data = json_decode($jsonPart, true);
+        $jsonPartvmess = base64_decode(substr($vmessLink, strpos($vmessLink, '://') + 3));
+        $datavmess = json_decode($jsonPartvmess, true);
 
-        if ($data !== null && isset($data['ps'])) {
-            $newName = implode(' | ', array_slice(explode(' | ', $data['ps']), 0, 2)) . ' |𓄂𓆃';
-            $data['ps'] = $newName;
-            $newJsonPart = base64_encode(json_encode($data));
+        if ($datavmess !== null && isset($datavmess['ps'])) {
+            $newNamevmess = implode(' | ', array_slice(explode(' | ', $datavmess['ps']), 0, 2)) . ' |𓄂𓆃';
+            $datavmess['ps'] = $newNamevmess;
+            $newJsonPartvmess = base64_encode(json_encode($datavmess));
 
-            return substr_replace($vmessLink, $newJsonPart, strpos($vmessLink, '://') + 3);
+            return substr_replace($vmessLink, $newJsonPartvmess, strpos($vmessLink, '://') + 3);
         }
 
         return $vmessLink;
     }
 
-    $contentLines = explode(PHP_EOL, $content);
-    foreach ($contentLines as &$line) {
+    $contentLinesvmess = explode(PHP_EOL, $contentvmess);
+    foreach ($contentLinesvmess as &$linevmess) {
         if (strpos($line, 'vmess://') === 0) {
-            $line = changeNameInVmessLink($line);
+            $linevmess = changeNameInVmessLink($linevmess);
         }
     }
-    unset($line);
+    unset($linevmess);
 
-    $headerSections = [
+    $headerSectionsvmess = [
         '#profile-title: base64:Vm1lc3PjgJjirLPwk4SC8JOGg+Kfv+OAmVZtZXNz',
         '#profile-update-interval: 1',
         '#subscription-userinfo: upload=0; download=0; total=10737418240000000; expire=2546249531',
         '#profile-web-page-url: https://github.com/darknessm427'
     ];
 
-    $content = implode(PHP_EOL, $headerSections) . PHP_EOL . implode(PHP_EOL, $contentLines);
+    $contentvmess = implode(PHP_EOL, $headerSectionsvmess) . PHP_EOL . implode(PHP_EOL, $contentLines);
 
     $writeResult = file_put_contents($filePath, $content);
 
     if ($writeResult !== false) {
-        echo 'Content successfully fetched and specified sections replaced. Content saved in ' . $filePath . '.';
+        echo 'Content successfully fetched and specified sections replaced. Content saved in ' . $filePathvmess . '.';
     } else {
         echo 'Error saving content to the file.';
     }
 } else {
     echo 'Error fetching content from the link.';
 }
+
 ?>
